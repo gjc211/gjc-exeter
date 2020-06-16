f6ec45c9789409c342b5b34a161f5342c45ef9ce
gjc211 <58478437+gjc211@users.noreply.github.com>
2020-06-16 14:58:37 +0100

    Add files via upload

--- S and T - Assignment 1.R
+++ S and T - Assignment 1.R
@@ -33,15 +33,19 @@ dupnew <- dup.coords(gdatanew)
 gdata2new <- jitterDupCoords(gdatanew,max=0.1,min=0.05)
 mlenew <- likfit(coords=gdata2new$coords,data=gdata2new$data, nugget = 0, ini=c(40,0.5), trend="1st", cov.model='exponential',kappa=3/2)
 
-plot(variog(gdata2,trend="1st"))
+plot(variog(gdata2,trend="1st"),max.dist = 6, ylim=c(0,30))
 lines(mle,col="red")
 lines(mle1,col="pink")
 lines(mle2,col="blue")
 lines(mle3,col="purple")
+legend(0,27, legend=c("Exponential","Spherical","Powered Exponential","Circular"),
+       col=c("red","pink","blue","purple"), lty=1, cex=0.8)
+legend(0,30,c(" "," "),title="Covariance Function",cex=1, bty='n')
+
+par(mfrow=c(1,1))
+plot(variog(gdata2new,trend="1st"),max.dist = 6, ylim=c(0,30))
 lines(mlenew,lty=5)
-legend(0,30, legend=c("Exponential","Spherical","Powered Exponential","Circular","Exponential Without Outliers"),
-       col=c("red","pink","blue","purple","black"), lty=1, cex=0.8)
-legend(0,32.5,c(" "," "),title="Covariance Function",cex=1, bty='n')
+
 
 # Plotting predicted values and variance
 grid <- pred_grid(gdata2$coords,by = 0.25)
@@ -63,14 +67,6 @@ plot(xv)
 
 # 4.
 
-# Kriging explanation: using the variogram to interpolate data - finding the best linear unbiased predictor 
-# for data point at each location based on the other data at that point in space (show forumula from L5)
-# Phi is used as the prior estimator - component of all possible variograms
-
-#head(parana)
-#parana.gr <- pred_grid(parana$borders, by = 15)
-
-
 ex.bayes <- krige.bayes(gdata2, coords=gdata2$coords, data=gdata2$data, loc=grid,
                         model = model.control(cov.m="exponential", kappa=2, trend.l = "1st", trend.d = "1st"),
                         prior = prior.control(phi = 1,
@@ -91,11 +87,11 @@ image(ex.bayes, val= "simulation", number.col=2,
 
 ex.bayes1 <- krige.bayes(gdata2, coords=gdata2$coords, data=gdata2$data, loc=grid,
                         model = model.control(cov.m="exponential", kappa=2, trend.l = "1st", trend.d = "1st"),
-                        prior = prior.control(phi.prior = "rec"))
+                        prior = prior.control(phi.prior = "rec",phi.discrete = seq(0,1,l=21)))
                                               
 par(mfrow=c(2,2), mar=c(4,4,2.5,0.5), mgp = c(2,1,0))
-image(ex.bayes1, main="predicted values")
-image(ex.bayes1, val="variance", main="prediction variance")
+image(ex.bayes1, main="Predicted values")
+image(ex.bayes1, val="variance", main="Prediction Variance")
 image(ex.bayes1, val= "simulation", number.col=1,
       main="Simulation from the \npredictive distribution (1)")
 image(ex.bayes1, val= "simulation", number.col=2,
@@ -106,9 +102,9 @@ image(ex.bayes1, val= "simulation", number.col=2,
 # Trying other models with a phi posterior distribution and nugget 
 
 ex.bayes2 <- krige.bayes(gdata2, coords=gdata2$coords, data=gdata2$data, loc=grid1,
-                         model = model.control(cov.m="gaussian", kappa=5/2, trend.l = "1st", trend.d = "1st", ),
-                         prior = prior.control(phi.prior = "rec",phi.discrete = seq(0,1,l=51),
-                                               tausq.rel.prior = "uni", tausq.rel.discrete = seq(3,4,l=6)))
+                         model = model.control(cov.m="gaussian", kappa=5/2, trend.l = "1st", trend.d = "1st"),
+                         prior = prior.control(phi.prior = "rec",phi.discrete = seq(0,1,l=21),
+                                               tausq.rel.prior = "uni", tausq.rel.discrete = seq(0.3,0.5,l=21)))
 
 par(mfrow=c(2,2), mar=c(4,4,2.5,0.5), mgp = c(2,1,0))
 image(ex.bayes2, main="Predicted values")
@@ -124,9 +120,9 @@ hist(ex.bayes2)
 ###
 
 ex.bayes3 <- krige.bayes(gdata2,coords=gdata2$coords, data=gdata2$data, loc=grid1,
-                         model = model.control(cov.m="matern", kappa=5/2, trend.l = "1st", trend.d = "1st", ),
+                         model = model.control(cov.m="matern", kappa=3/2, trend.l = "1st", trend.d = "1st", ),
                          prior = prior.control(phi.prior = "rec",phi.discrete = seq(0,1,l=21),
-                                               tausq.rel.prior = "uni", tausq.rel.discrete = seq(3,4,l=6)))
+                                               tausq.rel.prior = "uni", tausq.rel.discrete = seq(0.3,0.5,l=21)))
 
 quants <- function(x){quantile(x, prob = c(0.05, 0.5, 0.95))}
 lines(ex.bayes3, summ = quants, ty="l", lty=c(2,1,2), col=1)
@@ -134,36 +130,50 @@ lines(ex.bayes3, summ = quants, ty="l", lty=c(2,1,2), col=1)
 ### 
 
 ex.bayes4 <- krige.bayes(gdata2,coords=gdata2$coords, data=gdata2$data, loc=grid1,
-                         model = model.control(cov.m="powered.exponential", kappa=3/2, trend.l = "1st", trend.d = "1st", ),
+                         model = model.control(cov.m="powered.exponential", kappa=3/2, trend.l = "1st", trend.d = "1st"),
                          prior = prior.control(phi.prior = "uniform",phi.discrete = seq(0,1,l=21),
-                                               tausq.rel.prior = "fixed", tausq.rel = 3))
+                                               tausq.rel.prior = "uni", tausq.rel.discrete = seq(0.3,0.5,l=21)))
 
 ### Trying more data
 
 ex.bayes5 <- krige.bayes(gdata2,coords=gdata2$coords, data=gdata2$data, loc=grid2,
-                         model = model.control(cov.m="matern", kappa=3/2, trend.l = "1st", trend.d = "1st", ),
+                         model = model.control(cov.m="matern", kappa=3/2, trend.l = "1st", trend.d = "1st"),
                          prior = prior.control(phi.prior = "uniform",phi.discrete = seq(0,1,l=21),
-                                               tausq.rel.prior = "fixed", tausq.rel = 3))
+                                               tausq.rel.prior ="uni", tausq.rel.discrete = seq(0.3,0.5,l=21)))
 
 ###
 ex.bayes7 <- krige.bayes(gdata2,coords=gdata2$coords, data=gdata2$data, loc=grid2,
-                         model = model.control(cov.m="powered.exponential", kappa=3/2, trend.l = "1st", trend.d = "1st", ),
+                         model = model.control(cov.m="powered.exponential", kappa=3/2, trend.l = "1st", trend.d = "1st"),
                          prior = prior.control(phi.prior = "squared.reciprocal",phi.discrete = seq(0,1,l=21),
-                                               tausq.rel.prior = "uni", tausq.rel.discrete = seq(3,4,l=6)))
+                                               tausq.rel.prior = "uni", tausq.rel.discrete = seq(0.3,0.5,l=21)))
 
 # Prior and posterior plots
-par(mfrow=c(2,2))
-plot(ex.bayes7, type = "h", tausq.rel = FALSE, col=c("red", "blue"))
-title(main="Powered Exponential")
+par(mfrow=c(1,2))
+plot(ex.bayes1, type = "h", tausq.rel = FALSE, col=c("red", "blue"))
+title(main="Exponential")
 plot(ex.bayes2, type = "h", tausq.rel = FALSE, col=c("red", "blue"))
 title(main="Gaussian")
-plot(ex.bayes3, type = "h", tausq.rel = FALSE, col=c("red", "blue")) 
-title(main="Matern 5/2")
-plot(ex.bayes5, type = "h", tausq.rel = FALSE, col=c("red", "blue"))
-title(main="Matern 3/2")
 legend("topright", legend=c("Prior", "Posterior"),
-       col=c("red", "blue"),lty=1,xpd = T, inset = c(0,-0.5))
+       col=c("red", "blue"),lty=1,bty="n")
 
-# Explain about positive definite 
-# Show how priors influence posteriors - different from normal kriging 
 
+# How posterior parameter distributions change
+par(mfrow=c(1,1))
+plot(variog(gdata2, trend = "1st"),max.dist = 6, ylim=c(0,30))
+lines(ex.bayes1, summ = mean, col="purple") # Exponential
+lines(ex.bayes2, summ = mean, col="green") # Gaussian
+lines(ex.bayes3, summ = mean, col="red") # Matern
+lines(ex.bayes4, summ = mean, col="blue") # Powered Exponential
+legend(0,27, legend=c("Exponential","Gaussian","Matern","Powered Exponential"),
+       col=c("purple","green","red","blue"), lty=1, cex=0.8)
+legend(0,30,c(" "," "),title="Covariance Function",cex=1, bty='n')
+
+
+# Effect of prior
+par(mfrow=c(1,2))
+plot(ex.bayes4, type = "h", tausq.rel = FALSE, col=c("red", "blue")) 
+title(main="Uniform Prior")
+plot(ex.bayes7, type = "h", tausq.rel = FALSE, col=c("red", "blue"))
+title(main="Squared Reciprocal Prior")
+legend("topright", legend=c("Prior", "Posterior"),
+       col=c("red", "blue"),lty=1,bty="n")
